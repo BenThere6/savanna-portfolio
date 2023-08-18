af043acdf756e2f46ac37c86604000169fc4c882
BenThere <benjaminbirdsall@icloud.com>
2023-08-18 17:49:00 -0600

    auto adjust height fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 document.addEventListener("DOMContentLoaded",function() {
+    resizeAboutMeSection();
+})
+
+window.addEventListener("resize",function() {
+    resizeAboutMeSection();
+})
+
+function resizeAboutMeSection() {
     aboutMeSection = document.getElementById('about-me-section');
     pars = document.querySelectorAll('.pars');
 
-    let totalRelativeOffset = 0;
+    let totalHeight = 0;
+    let count = 0;
     pars.forEach((par) => {
-        totalRelativeOffset += Math.abs(parseFloat(par.style.top) || 0);
-    })
-    aboutMeSection.style.height = `calc(100% - ${totalRelativeOffset}px)`;
-})
-
+        count ++;
+        // console.log(totalHeight)
+        // console.log(par.clientHeight)
+        // console.log(par)
+        if (count != 1) {
+            console.log(totalHeight)
+            totalHeight += par.clientHeight;
+        }
+        
+    });
+    totalHeight += 650;
+    aboutMeSection.style.height = `${totalHeight}px`;
+}
