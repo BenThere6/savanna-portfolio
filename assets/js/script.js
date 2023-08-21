c6938e9176ade8ff8b1572ac8c17e30c7e11677c
BenThere <benjaminbirdsall@icloud.com>
2023-08-20 18:53:29 -0600

    fixed media query/ clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,9 @@ const parentElement = document.querySelector('#work-section');
 const childElement = document.querySelector('#proj-one');
 
 function updateChildPosition() {
-    console.log(`Child Top Value: ${getComputedStyle(childElement).getPropertyValue('top')}`)
     const parentWidth = parentElement.clientWidth;
-    console.log(`Parent Width: ${parentWidth}`)
     const topValue = parentWidth * 0.015;
-    console.log(`New Top Value: ${topValue}`)
     childElement.style.top = `${topValue}px`;
-    console.log(`Child Top Value: ${getComputedStyle(childElement).getPropertyValue('top')}`)
 }
 
 updateChildPosition();
