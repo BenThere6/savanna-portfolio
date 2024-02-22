4d2355c0b14b9b9cb6d42eb3a638cc9e2ce0c961
BenThere6 <benjaminbirdsall@icloud.com>
2024-02-21 22:11:01 -0700

    changes

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Header from './pages/Home'
+
+function App() {
+    return <Header />;
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-const App = () => {
-  return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </Layout>
-    </Router>
-  );
-};
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
