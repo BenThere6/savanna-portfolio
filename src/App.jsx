3f61f0945d1102a1073c30f5224c799fba8caddc
BenThere <benjaminbirdsall@icloud.com>
2024-02-24 14:38:43 -0700

    set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import Header from './pages/Home'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
+function Layout() {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+}
 
 function App() {
-    return <Header />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="projects" element={<Projects />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
