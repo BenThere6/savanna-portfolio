7d875dfd099b4858b889e99483e1cf1eaf55e582
BenThere <benjaminbirdsall@icloud.com>
2024-02-24 14:59:19 -0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Your Website Name. All rights reserved.</p>
-        {/* Add any additional footer content or links here */}
+        <p>&copy; {new Date().getFullYear()} Ben Birdsall</p>
       </div>
     </footer>
   );
