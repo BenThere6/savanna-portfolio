b16960042d1636d6284595656d9106d62dfbf041
BenThere <benjaminbirdsall@icloud.com>
2024-02-25 00:34:44 -0700

    add habit-tracker image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import project_image from '../assets/project-image.png';
+import habit_tracker from '../assets/habit-tracker.png';
 
 const Projects = () => {
   return (
@@ -9,7 +10,23 @@ const Projects = () => {
         <h2>My Projects</h2>
         <Nav />
         <div className="project" id='first-project'>
-          <h3 className='project-name'>Project 1</h3>
+          <h3 className='project-name'>Habit Tracker</h3>
+          <div className='center'>
+            <img className='project-image' src={habit_tracker} alt='project-image'></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+          </p>
+          <div className='center'>
+            <div className='gray-container project-links'>
+              <a href="#">View Project</a>
+              <a href="#">View Repository</a>
+            </div>
+          </div>
+        </div>
+        <div className="project">
+          <h3 className='project-name'>Movie Night Coordinator</h3>
           <div className='center'>
             <img className='project-image' src={project_image} alt='project-image'></img>
           </div>
@@ -25,7 +42,7 @@ const Projects = () => {
           </div>
         </div>
         <div className="project">
-          <h3 className='project-name'>Project 1</h3>
+          <h3 className='project-name'>List Prioritizer</h3>
           <div className='center'>
             <img className='project-image' src={project_image} alt='project-image'></img>
           </div>
@@ -41,7 +58,7 @@ const Projects = () => {
           </div>
         </div>
         <div className="project">
-          <h3 className='project-name'>Project 1</h3>
+          <h3 className='project-name'>Quiz App</h3>
           <div className='center'>
             <img className='project-image' src={project_image} alt='project-image'></img>
           </div>
@@ -52,6 +69,21 @@ const Projects = () => {
           <div className='center'>
             <div className='gray-container project-links'>
               <a href="#">View Project</a>
+              <a href="#">View Repository</a>
+            </div>
+          </div>
+        </div>
+        <div className="project">
+          <h3 className='project-name'>Auto Budgeter</h3>
+          <div className='center'>
+            <img className='project-image' src={project_image} alt='project-image'></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+          </p>
+          <div className='center'>
+            <div className='gray-container project-links'>
               <a href="#">View Repository</a>
             </div>
           </div>
