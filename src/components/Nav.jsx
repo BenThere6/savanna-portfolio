6f5bf6bf0c641cc62cd75e1423d8d88f16e83ae4
BenThere <benjaminbirdsall@icloud.com>
2024-02-25 17:22:35 -0700

    style sticky nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
-    <div className='gray-container nav'>
+    <div className={`gray-container nav ${isHomePage ? 'nav-shifted' : ''}`}>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </div>
   );
