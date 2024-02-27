851a65564f7f0aa94c35598e1e6bdfd0033209a3
BenThere6 <benjaminbirdsall@icloud.com>
2024-02-26 21:12:01 -0700

    add columns to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,95 +5,97 @@ import habit_tracker from '../assets/habit-tracker.png';
 
 const Projects = () => {
   return (
-    <div className="projects page">
+    <div className="page">
       <div className="page-content">
         <h2>My Projects</h2>
         <Nav />
+        <div className='projects-container'>
 
-        <div className="project" id='first-project'>
-          <h3 className='project-name'>Habit Tracker</h3>
-          <div className='center'>
-            <img className='project-image' src={habit_tracker} alt='project-image'></img>
-          </div>
-          <p className='gray-text-container'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
-            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
-          </p>
-          <div className='center'>
-            <div className='gray-container project-links'>
-              <a href="#">View Project</a>
-              <a href="#">View Repository</a>
+          <div className="project" id='proj-one'>
+            <h3 className='project-name'>Habit Tracker</h3>
+            <div className='center'>
+              <img className='project-image' src={habit_tracker} alt='project-image'></img>
+            </div>
+            <p className='gray-text-container'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+              Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+            </p>
+            <div className='center'>
+              <div className='gray-container project-links'>
+                <a href="#">View Project</a>
+                <a href="#">View Repository</a>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="project">
-          <h3 className='project-name'>Movie Night Coordinator</h3>
-          <div className='center'>
-            <img className='project-image' src={project_image} alt='project-image'></img>
-          </div>
-          <p className='gray-text-container'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
-            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
-          </p>
-          <div className='center'>
-            <div className='gray-container project-links'>
-              <a href="#">View Project</a>
-              <a href="#">View Repository</a>
+          <div className="project" id='proj-two'>
+            <h3 className='project-name'>Movie Night Coordinator</h3>
+            <div className='center'>
+              <img className='project-image' src={project_image} alt='project-image'></img>
+            </div>
+            <p className='gray-text-container'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+              Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+            </p>
+            <div className='center'>
+              <div className='gray-container project-links'>
+                <a href="#">View Project</a>
+                <a href="#">View Repository</a>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="project">
-          <h3 className='project-name'>List Prioritizer</h3>
-          <div className='center'>
-            <img className='project-image' src={project_image} alt='project-image'></img>
-          </div>
-          <p className='gray-text-container'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
-            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
-          </p>
-          <div className='center'>
-            <div className='gray-container project-links'>
-              <a href="#">View Project</a>
-              <a href="#">View Repository</a>
+          <div className="project">
+            <h3 className='project-name'>List Prioritizer</h3>
+            <div className='center'>
+              <img className='project-image' src={project_image} alt='project-image'></img>
+            </div>
+            <p className='gray-text-container'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+              Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+            </p>
+            <div className='center'>
+              <div className='gray-container project-links'>
+                <a href="#">View Project</a>
+                <a href="#">View Repository</a>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="project">
-          <h3 className='project-name'>Quiz App</h3>
-          <div className='center'>
-            <img className='project-image' src={project_image} alt='project-image'></img>
-          </div>
-          <p className='gray-text-container'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
-            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
-          </p>
-          <div className='center'>
-            <div className='gray-container project-links'>
-              <a href="#">View Project</a>
-              <a href="#">View Repository</a>
+          <div className="project">
+            <h3 className='project-name'>Quiz App</h3>
+            <div className='center'>
+              <img className='project-image' src={project_image} alt='project-image'></img>
+            </div>
+            <p className='gray-text-container'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+              Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+            </p>
+            <div className='center'>
+              <div className='gray-container project-links'>
+                <a href="#">View Project</a>
+                <a href="#">View Repository</a>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="project">
-          <h3 className='project-name'>Auto Budgeter</h3>
-          <div className='center'>
-            <img className='project-image' src={project_image} alt='project-image'></img>
-          </div>
-          <p className='gray-text-container'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
-            Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
-          </p>
-          <div className='center'>
-            <div className='gray-container project-links'>
-              <a href="#">View Repository</a>
+          <div className="project">
+            <h3 className='project-name'>Auto Budgeter</h3>
+            <div className='center'>
+              <img className='project-image' src={project_image} alt='project-image'></img>
+            </div>
+            <p className='gray-text-container'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in aliquam purus.
+              Integer euismod leo nec risus fermentum, ac eleifend velit consequat.
+            </p>
+            <div className='center'>
+              <div className='gray-container project-links'>
+                <a href="#">View Repository</a>
+              </div>
             </div>
           </div>
-        </div>
 
+        </div>
       </div>
     </div>
   );
