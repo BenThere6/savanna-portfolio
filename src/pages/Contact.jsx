bb4acfadaa3c08f2b19406016bf951d2f1ea104d
BenThere <benjaminbirdsall@icloud.com>
2024-02-26 23:33:34 -0700

    handle form submissions

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import Nav from '../components/Nav';
 
 const ContactPage = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_45wr2uk', 'template_y1pnsx8', form.current, 'YOUR_USER_ID')
+      .then((result) => {
+        console.log('Email successfully sent!', result.text);
+        form.current.reset();
+      }, (error) => {
+        console.log('Failed to send email:', error.text);
+      });
+  };
+
   return (
     <div className="contact page">
       <div className='page-content'>
@@ -10,11 +25,11 @@ const ContactPage = () => {
         <p className='gray-text-container'>
           If you have any questions or just want to say hello, feel free to contact me using the form below or by sending an email.
         </p>
-        <form className='gray-container contact-form'>
+        <form className='gray-container contact-form' onSubmit={sendEmail} ref={form}>
           <input className="form-field" type="text" id="name" name="name" placeholder="Name" />
           <input className="form-field" type="email" id="email" name="email" placeholder="Email" />
-          <input className="form-field" id="subject" name="subject" placeholder="Subject"></input>
-          <textarea className="form-field" id="message" name="message" placeholder="Message"></textarea>
+          <input className="form-field" id="subject" name="subject" placeholder="Subject" />
+          <textarea className="form-field" id="message" name="message" placeholder="Message" />
           <button className="form-field" id="submit" type="submit">Send Message</button>
         </form>
       </div>
