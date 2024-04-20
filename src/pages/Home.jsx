1940c2277025fbb18aaa2634d6cb9a231bd13a9e
Ben Birdsall <benjaminbirdsall@icloud.com>
2024-04-20 13:48:53 -0600

    update position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className='home-content'>
         <Nav />
         <h2 id='my-name'>Savanna Birdsall</h2>
-        <h3 >Full Stack Web Developer</h3>
+        <h3 >Child Relations Professional</h3>
       </div>
     </div>
   );
