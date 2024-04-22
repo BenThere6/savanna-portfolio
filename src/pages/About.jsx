f29c1bacced1be68e86290dc6671fe771eb0a70e
BenThere <benjaminbirdsall@icloud.com>
2024-04-22 16:58:38 -0600

    add fonts and resume section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
   return (
     <div className="about page">
       <div className='page-content'>
-        <h2>About Me</h2>
+        <div>
+          <h2 className='white-fade-background'>About Me</h2>
+        </div>
         <Nav />
         <div id="headshot-circle">
           <img id='headshot' src={headshot} alt="Photo of Ben Birdsall" />
@@ -17,6 +19,33 @@ const About = () => {
           </p>
           <p>
             Outside of academics, I'm deeply passionate about singing, dancing, and reading. Music and movement are my creative outlets, allowing me to express myself freely, while books offer me endless opportunities for learning and exploration. I thrive on new experiences and connections, always seeking to grow and embrace the richness of life.
+          </p>
+        </div>
+        <h3 id='resume-title'>Resume</h3>
+        <div className='p-container gray-text-container'>
+          <h4>
+            Education
+          </h4>
+          <p>
+            This is the education
+          </p>
+          <h4>
+            Work Experience
+          </h4>
+          <p>
+            This is the work experience
+          </p>
+          <h4>
+            Skills
+          </h4>
+          <p>
+            This is the skill section
+          </p>
+          <h4>
+            Certifications
+          </h4>
+          <p>
+            This is the certifications section
           </p>
         </div>
       </div>
