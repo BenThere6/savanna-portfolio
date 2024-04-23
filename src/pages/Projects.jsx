cfff678cb12fb608520e31e486d14004efa5ef95
BenThere <benjaminbirdsall@icloud.com>
2024-04-22 20:33:37 -0600

    linked project images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import template_image from '../assets/template-project-image.png';
+import feature_story_image from '../assets/feature-story-image.png';
+import meltwater_image from '../assets/Meltwater-image.png';
+import muckrack_image from '../assets/muckrack-image-2.png';
+import news_release_image from '../assets/news-release-image.png';
+import oped_image from '../assets/oped-image.png';
+import semrush_image from '../assets/SEMrush-image.png';
 
 const Projects = () => {
   return (
@@ -15,7 +21,7 @@ const Projects = () => {
           <div className="project" id='proj-one'>
             <h3 className='project-name'>Muck Rack Project</h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={muckrack_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>In this project, I analyzed MuckRack data on mindfulness and meditation to determine 5 reporters I would reach out to if I worked for a company centered on meditation. I then created an event the company would sponsor, in order to meet the reporters, and give them an opportunity to learn about our company for future opportunities.</p>
@@ -37,7 +43,7 @@ const Projects = () => {
           <div className="project">
             <h3 className='project-name'>Melt Water Project</h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={meltwater_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>In this project, I acted in the Role of Our Family Wizard, a company that facilitates co-parenting communication among separated parents. Through my research, I learned what time of year OFW receives the most coverage. I learned that its overall sentiment is neutral, and I learned which major outlets cover it most. As a PR representative, I can use this information to help the company strategically plan ways to communicate with their publics in order to reach KPI's.</p>
@@ -59,7 +65,7 @@ const Projects = () => {
           <div className="project">
             <h3 className='project-name'>SEMrush Project</h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={semrush_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>Through this project, I used SEMrush to analyze Our Family Wizard's backlinks and keywords. I compared those with the most effective keywords related to Our Family Wizard's work, and offered suggestions to help Our Family Wizard's Search Engine Optimization. This would increase the public awareness of the company, and help the publics understand the resource OFW offers.</p>
@@ -83,7 +89,7 @@ const Projects = () => {
           <div className="project">
             <h3 className='project-name'>Op Ed: The Perfect Lie </h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={oped_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>This is a challenge to perfectionism, and the mindsets that poison us over time. In this Op Ed, I help people recognize perfectionistic tendencies. I also offer simple steps to help those who struggle with perfectionism begin to heal. Many people think it's all-or-nothing. But what could a little step here, a little step there accomplish over the long run?</p>
@@ -105,7 +111,7 @@ const Projects = () => {
           <div className="project">
             <h3 className='project-name'>Feature Story: Sued and Sales Doubled: A PR victory</h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={feature_story_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>In this piece, I explore the story of how a brand new business was sued by a major cookie corporation, and used it to double sales.</p>
@@ -127,7 +133,7 @@ const Projects = () => {
           <div className="project">
             <h3 className='project-name'>News Release: Freeman Habrowski speaks at BYU</h3>
             <div className='center'>
-              <img className='project-image' src={template_image} alt='project-image'></img>
+              <img className='project-image' src={news_release_image} alt='project-image'></img>
             </div>
             <div className='gray-text-container proj-paragraph'>
               <p>In this project, I wrote from the perspective of BYU's Public Relations Team, offering a news release to help students anticipate Freeman Habrowski coming to speak to BYU students in February 2024.</p>
