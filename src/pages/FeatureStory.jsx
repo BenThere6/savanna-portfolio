531541b1ceda7715195f3afa283ce4162c1390ae
BenThere <benjaminbirdsall@icloud.com>
2024-06-28 22:13:34 -0600

    add writing tactics pages

---
+++ src/pages/FeatureStory.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const FeatureStory = () => {
+    return (
+        <div className="page-content">
+            <Nav />
+            <div className='ethics-page'>
+                
+            </div>
+        </div>
+    );
+};
+
+export default FeatureStory;
---
+++ src/pages/NewsRelease.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const NewsRelease = () => {
+    return (
+        <div className="page-content">
+            <Nav />
+            <div className='ethics-page'>
+                
+            </div>
+        </div>
+    );
+};
+
+export default NewsRelease;
