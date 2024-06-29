cd712a208753cf7f915bece18a1dc73e912c775f
BenThere <benjaminbirdsall@icloud.com>
2024-06-28 22:50:01 -0600

    add routes for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import PersonalEthicalStatement from './pages/PersonalEthicalStatement';
+import ThePerfectLie from './pages/ThePerfectLie';
+import FeatureStory from './pages/FeatureStory';
+import NewsRelease from './pages/NewsRelease';
 
 function Layout() {
     return (
@@ -26,7 +29,10 @@ function App() {
                     <Route path="about" element={<About />} />
                     <Route path="contact" element={<Contact />} />
                     <Route path="projects" element={<Projects />} />
-                    <Route path="ethical-statement" element={<PersonalEthicalStatement />} /> {/* Add this line */}
+                    <Route path="ethical-statement" element={<PersonalEthicalStatement />} />
+                    <Route path="the-perfect-lie" element={<ThePerfectLie />} />
+                    <Route path="feature-story" element={<FeatureStory />} />
+                    <Route path="news-release" element={<NewsRelease />} />
                 </Route>
             </Routes>
         </BrowserRouter>
